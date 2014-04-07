a2501935d573771fdb96b8974df973d641e8206a
dibley1973 <dibley1973@yahoo.co.uk>
2014-04-07 07:36:54 +0100

    20140407-0736-DWLT Found some unstaged changes

--- Dibware.Helpers/Validation/Guard.cs
+++ Dibware.Helpers/Validation/Guard.cs
@@ -26,6 +26,23 @@ namespace Dibware.Helpers.Validation
         }
 
         /// <summary>
+        /// Checks if the specified string is not null or empty.
+        /// If it is throws a System.ArgumentNullException
+        /// </summary>
+        /// <example>Guard.ArgumentIsNotNull(arg1, "arg1");</example>
+        /// <param name="value">The value.</param>
+        /// <param name="argumentName">The argument name.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        [DebuggerHidden]    //Does not appear at all in the call stack
+        public static void ArgumentIsNotNullOrEmpty(String value, String argumentName)
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        /// <summary>
         /// Checks if the specified condition is true. If it is 
         /// a System.ArgumentOutOfRangeException is thrown with the 
         /// argument name
@@ -64,6 +81,21 @@ namespace Dibware.Helpers.Validation
             if (condition)
             {
                 throw new ArgumentOutOfRangeException(argumentName, message);
+            }
+        }
+
+        /// <summary>
+        /// Invalids the operation.
+        /// </summary>
+        /// <param name="condition">if set to <c>true</c> [condition].</param>
+        /// <param name="message">The message.</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        [DebuggerHidden]    //Does not appear at all in the call stack
+        public static void InvalidOperation(Boolean condition, String message)
+        {
+            if (condition)
+            {
+                throw new InvalidOperationException(message);
             }
         }
     }
