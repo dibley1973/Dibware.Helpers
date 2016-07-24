bc5902a040cde6ffd8fd0dfa786a5155bcc6b826
dibley1973 <dibley1973@yahoo.co.uk>
2016-07-24 06:54:27 +0100

    20160724_0654_DWLT Added NuGet Package

---
+++ Tests/Dibware.Helpers.PackageLoadTester/PackageLoadTest.cs
@@ -0,0 +1,17 @@
+﻿
+using Dibware.Helpers.Validation;
+
+namespace Dibware.Helpers.PackageLoadTester
+{
+    public class PackageLoadTest
+    {
+        public void GuardTest(int param1, string param2)
+        {
+            Guard.ArgumentOutOfRange(param1 <= 0, "param1");
+            Guard.ArgumentIsNotNullOrEmpty(param2, "param2");
+
+            var value2 = Ensure.ArgumentIsNotNullOrEmpty(param2, "param2");
+
+        }
+    }
+}
--- Tests/Dibware.Helpers.PackageLoadTester/Properties/AssemblyInfo.cs
+++ Tests/Dibware.Helpers.PackageLoadTester/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dibware.Helpers")]
-[assembly: AssemblyDescription("A small library of helper classes")]
+[assembly: AssemblyTitle("Dibware.Helpers.PackageLoadTester")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dibware")]
-[assembly: AssemblyProduct("Dibware.Helpers")]
-[assembly: AssemblyCopyright("Copyright ©  2016 Dibware")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Dibware.Helpers.PackageLoadTester")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f4ce899a-a656-45b5-89ad-6caae4747322")]
+[assembly: Guid("bc5f6fd8-b1d5-461e-8afb-20099ac748e4")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.*")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
